1e4c345c92d87f3ad952f7022aec2f0c84fec574
nicoftheshire <nholtzhausen54@gmail.com>
2022-01-25 13:12:49 +0200

    stringDifference.py completed

--- stringDifference.py
+++ stringDifference.py
@@ -3,7 +3,22 @@
 # one extra character. The function returns that character.
 
 def difference(str1, str2):
-    for i in range(len(str1)):
-        for j in range(len(str2)):
-            if str1[i] == str2[j]:
+    i = 0
+    j = 0
+    while i < len(str2):
+        while j < len(str1):
+            if not (str2[i] == str1[j]):
+                if j == (len(str1) - 1):
+                    return str2[i]
+                j += 1
                 break
+            else:
+                i += 1
+                j = 0
+                break
+
+def main():
+    res = difference('eueiieo', 'iieoedue')
+    print(res)
+
+main()
